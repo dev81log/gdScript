ee70a8d8a112a71a30086de8b7164f70e9165758
dev81log <dev81log@gmail.com>
2023-11-04 13:38:05 -0300

    Add health and attack damage variables, death animation, and hit animation

--- tiny_swords/knight/knight.gd
+++ tiny_swords/knight/knight.gd
@@ -1,15 +1,23 @@
 extends CharacterBody2D
 
-@onready var animation: AnimationPlayer = get_node("Animation")
-@onready var texture: Sprite2D = get_node("Texture")
 
+@onready var texture: Sprite2D = get_node("Texture")
+@onready var animation: AnimationPlayer = get_node("Animation")
+@onready var animation_aux: AnimationPlayer = get_node("AnimationAux")
+@onready var attack_area_collision: CollisionShape2D = get_node("AttackArea/Collision")
+
+
+@export var health: int = 10
 @export var move_speed: float = 256.0
+@export var attack_damage: int = 1
+
 
 var can_attack: bool = true
+var can_die: bool = false
 
 
 func _physics_process(_delta: float) -> void:
-	if can_attack == false:
+	if can_attack == false or can_die == true:
 		if animation.is_playing() == false:
 			can_attack = true
 		return
@@ -42,6 +50,14 @@ func animate() -> void:
 	animation.play("idle")
 
 
+func _on_animation_finished(anim_name: String) -> void:
+	match anim_name:
+		"attack":
+			can_attack = true
+		"death":
+			get_tree().reload_current_scene()
+
+
 func attack_handler() -> void:
 	if Input.is_action_just_pressed("attack") and can_attack:
 		can_attack = false
@@ -51,3 +67,27 @@ func attack_handler() -> void:
 func handle_flip(direction: Vector2) -> void:
 	if direction.x != 0:
 		texture.flip_h = direction.x < 0
+
+	if texture.flip_h:
+		attack_area_collision.position.x = -60
+	else:
+		attack_area_collision.position.x = 60
+
+
+func on_attack_area_body_entered(body) -> void:
+	body.update_health(attack_damage)
+
+
+func update_health(value: int) -> void:
+	self.health -= value
+
+	if self.health <= 0:
+		can_die = true
+		animation.play("death")
+		attack_area_collision.set_deferred("disable", true)
+		return
+
+	animation_aux.play("hit")
+
+
+
